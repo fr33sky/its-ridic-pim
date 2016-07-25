7cb543cd82a32d412f24e07dfc2aee7c799240c8
Nate Dalo <nate.dalo@yahoo.com>
2016-07-25 15:42:39 -0500

    fix typo in migration

--- db/migrate/20160725202047_remove_expense_account_id_from_expenses.rb
+++ db/migrate/20160725202047_remove_expense_account_id_from_expenses.rb
@@ -1,6 +1,6 @@
 class RemoveExpenseAccountIdFromExpenses < ActiveRecord::Migration
   def change
-    if column_exist?(:expense, :expense_account_id)
+    if column_exists?(:expense, :expense_account_id)
       remove_column :expenses, :expense_account_id
     end
   end
