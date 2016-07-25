103c9eece9ffbec861f4c53d06e3aeb74de92929
Nate Dalo <nate.dalo@yahoo.com>
2016-07-25 15:29:41 -0500

    remove references to accounts

--- db/migrate/20160614173353_create_expenses.rb
+++ db/migrate/20160614173353_create_expenses.rb
@@ -1,7 +1,6 @@
 class CreateExpenses < ActiveRecord::Migration
   def change
     create_table :expenses do |t|
-      t.references :accounts, index: true, foreign_key: true
       t.string :description
       t.float :amount
 
