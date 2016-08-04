99148d3f6367011d8d2f56a9f246d92907d6fd67
Nate Dalo <nate.dalo@yahoo.com>
2016-08-03 22:11:38 -0500

    Add default of 0 for last_order_average_cost

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,7 +5,11 @@ class OrderItem < ActiveRecord::Base
 
 
   def last_order_average_cost
-    OrderItem.joins(:order).where("product_id = ? AND user_date < ?", self.product.id, self.order.user_date).order("user_date DESC").first.average_cost
+    begin
+      return OrderItem.joins(:order).where("product_id = ? AND user_date < ?", self.product.id, self.order.user_date).order("user_date DESC").first.average_cost
+    rescue
+      return 0.00
+    end
   end
 
   def last_quantity(oi=self)
