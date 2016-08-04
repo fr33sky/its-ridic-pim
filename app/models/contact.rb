3634465066cb601949cf5e8e8a5e5ad6cd136982
Nate Dalo <nate.dalo@yahoo.com>
2016-08-04 09:55:47 -0500

    Added email address to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email_address, presence: true, length: { maximum: 255 },
+                          format: { with: VALID_EMAIL_REGEX }
   def to_s
     name
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725202158) do
+ActiveRecord::Schema.define(version: 20160804144459) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -74,9 +74,10 @@ ActiveRecord::Schema.define(version: 20160725202158) do
     t.string   "state"
     t.string   "postal_code"
     t.string   "country"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "qbo_id"
+    t.string   "email_address"
   end
 
   create_table "credentials", force: :cascade do |t|
