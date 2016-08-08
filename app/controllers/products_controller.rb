b5e833173968d33bf0d5d890c5e3ea430f510c9c
Nate Dalo <nate.dalo@yahoo.com>
2016-08-08 15:07:30 -0500

    Add AJAX for destroy option in products/index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,6 +59,7 @@ class ProductsController < ApplicationController
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.js {}
       format.json { head :no_content }
     end
   end
